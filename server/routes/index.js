38aaa36902bab3373fa0b126e66bb0b7212b832c
Giovanni annaloro <giovanniannaloro3x5@gmail.com>
2023-02-11 14:11:20 +0000

    Adattamento

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,44 @@ router.get('/logout',function(req, res, next){
 });
 
 
+router.post('/iscriviti/:idp',function(req,res,next){
+  executeQuery(`select * from utenti where idu in (select idu from partecipazioni where idp = '${req.params.idp}') `, function(error,results) {
+    if(error) throw error; 
+    executeQuery(`select npm from partite where idp = '${req.params.idp}' `, function(er,res) {
+      if(er) throw er;
+      if (parseInt(res[0].npm) = length(results)){return res.send("La partita ha già raggiunto il numero massimo di partecipanti");}
+    });
+    if (req.session.type == "p"){
+    executeQuery(`insert into partecipazioni(idp,idu) values('${req.params.idp}','${req.session.idu}')`,function(rer,ris){
+      res.send("Iscrizione effettuata");})}
+    else {return res.send("Devi loggarti come partecipante per iscriverti ad un evento");}
+    });
+    //res.render('iscriviti.pug', {partecipanti : results});
+  });
+//});
+
+router.get('/iscriviti/:idp',function(req,res,next){
+  executeQuery(`select * from utenti where idu in (select idu from partecipazioni where idp = '${req.params.idp}') `, function(error,results) {
+    if(error) throw error; 
+    res.render('iscriviti.pug', {partecipanti : results});
+  });
+});
+
+router.get('/events',function(req,res,next){
+  executeQuery("select * from partite ", function(error,results) {
+    if(error) throw error; 
+    res.render('events.pug', {events: results});
+  });
+});
+
+router.get('/event/:idp',function(req,res,next){
+  executeQuery(`select * from partite where idp = '${req.params.idp}' `, function(error,results) {
+    if(error) throw error; 
+    res.render('event.pug', {event: results[0]});
+  });
+});
+
+
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
@@ -30,7 +68,7 @@ router.get('/contatti',function(req, res, next){
 
 
 router.get('/registrazione',function(req, res, next){
-  res.render('registrazione', {title: 'Login'});
+  res.render('registrazione', {title: 'Registrazione'});
 });
 
 router.post('/registrazione',function(req, res, next){
@@ -40,7 +78,11 @@ router.post('/registrazione',function(req, res, next){
       if(req.body.rpassword!=req.body.password){res.send("password diverse");}
       else {}
     });
-    executeQuery(`insert into utenti(id,mail,password) values('${req.body.id}','${req.body.mail}','${req.body.password}')`,function(rer,ris){
+    executeQuery(`select mail from utenti where idu = '${req.body.idu}'`,function(error,results){
+      if(results.length>0){res.send("id  già esistente");}
+      else {}
+    });
+    executeQuery(`insert into utenti(nome,cognome,mail,idu,username,tipo,password) values('${req.body.nome}','${req.body.cognome}','${req.body.mail}','${req.body.idu}','${req.body.username}','${req.body.tac}','${req.body.password}')`,function(rer,ris){
       res.send("utente registrato");
     });
   });
@@ -59,6 +101,21 @@ router.post('/registrazione',function(req, res, next){
       res.render('users', {users: results});
     });
   });
+
+  router.get('/create-evento',function(req, res, next){
+    res.render('create-evento', {title: 'Creazione evento'});
+  });
+  
+  router.post('/create-evento',function(req, res, next){
+      executeQuery(`select mail from utenti where mail = '${req.body.mail}'`,function(error,results){
+        if (req.session.user != null && req.session.type=="o"){
+          executeQuery(`insert into partite (idp,sport,npm,luogo,ora,data) values('${req.body.idp}','${req.body.sport}','${req.body.npm}','${req.body.luogo}','${req.body.ora}','${req.body.data}')`,function(rer,ris){
+            res.send("Evento creato ");
+          });
+        }
+        else {return res.send("Devi essere un organizzatore per creare un evento");}
+      });
+    });
   
 
 module.exports = router;
--- server/routes/session.js
+++ server/routes/session.js
@@ -11,12 +11,14 @@ router.use(session({ secret:'ruguhd' }));
 
 
   router.post('/', function(req, res, next){
-    executeQuery(`select id from utenti where mail = '${req.body.mail}' and password = '${req.body.password}'`,function(error,result){
+    executeQuery(`select * from utenti where mail = '${req.body.mail}' and password = '${req.body.password}'`,function(error,result){
       if(error) throw error;
       if (result.length == 0){res.send("Username o password non corretti");}
       else{
         //creazione sessione
         req.session.user = req.body.mail;
+        req.session.type = result[0].tipo;
+        req.session.idu = result[0].idu;
         return res.redirect("/session/account");
       }    
     })
